51c86723439f15ec423e58b1352c87732be7cd56
Igiz <igizstarkle@gmail.com>
2024-09-17 22:03:28 +0300

    update the modal, close, open

--- app/Livewire/Posts.php
+++ app/Livewire/Posts.php
@@ -6,11 +6,29 @@ use Livewire\Component;
 
 class Posts extends Component
 {
-   
-   
-   
+    public $posts, $title, $body, $post_id;
+    public $isOpen = 0;
+
     public function render()
     {
         return view('livewire.posts');
     }
+
+    public function create(){
+        $this->resetInputFields();
+        $this->openModal();
+    }
+
+    public function openModal(){
+        $this->isOpen = true;
+    }
+
+    public function closeModal(){
+        $this->isClose = false;
+    }
+
+    
+
+
+
 }
