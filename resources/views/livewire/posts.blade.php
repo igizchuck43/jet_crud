77dcf25be91a07bfc4b95013876ccb66a879288e
Igiz <igizstarkle@gmail.com>
2024-09-18 11:03:17 +0300

    Create blade component

--- resources/views/livewire/posts.blade.php
+++ resources/views/livewire/posts.blade.php
@@ -7,6 +7,57 @@
 <x-slot>
 
 
-<div>
+<div class=py-12>
+
+    <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+        <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg px-4 py-4">
+            @if(session()->has('message'))
+            <div class="bg-teal-100 border-t-4 border-teal-500 rounded-b text-teal-900 px-4 py-4 shadow-md my-3">
+                <div class="flex">
+                    <div class="">
+                        <p class="text-sm">
+                            {{ session('message') }}
+                        </p>
+                    </div>
+                </div>
+            </div>
+            @endif
+            <button wire:click='create' class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded my-3">
+                Create New Post
+            </button>
+
+            @if($isOpen)
+                @include('livewire.create')
+            @endif
+
+            <table class="table-fixed w-full">
+                <thead>
+                    <tr class="bg-gray-100">
+                        <th class="px-4 py-2 w-20">No.</th>
+                        <th class="px-4 py-2">Title</th>
+                        <th class="px-4 py-2">Body</th>
+                        <th class="px-4 py-2">Action</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach ($posts as $post)
+                       <tr>
+                        <td class="border px-4 py-2">{{ $post->id }}</td>
+                        <td class="border px-4 py-2">{{ $post->title }}</td>
+                        <td class="border px-4 py-2">{{ $post->body }}</td>
+                        <td class="border px-4 py-2">
+                            <button wire:click='edit({{$post->id}})' class="bg-blue-500 hover:bg-blue-700 text-white  font-bold py-2 px-4 rounded">
+                                Edit
+                            </button>
+                            <button wire:click='edit({{$post->id}})' class="bg-red-500 hover:bg-bl-red-700 text-white font-bold py-2 px-4 rounded">
+                                Delete
+                            </button>
+                        </td>
+                       </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        </div>
+    </div>
     {{-- Because she competes with no one, no one can compete with her. --}}
 </div>
