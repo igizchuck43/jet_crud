ae9af973832e7c71f3ee84c03e311601f4b6af99
Igiz <igizstarkle@gmail.com>
2024-09-18 10:40:41 +0300

    x-slot blade

--- resources/views/livewire/posts.blade.php
+++ resources/views/livewire/posts.blade.php
@@ -1,3 +1,12 @@
+<x-slot name=header>
+
+    <h2 class='font-semibold text-xl text-gray-800 leading-tight'>
+        Manage Posts
+    </h2>
+
+<x-slot>
+
+
 <div>
     {{-- Because she competes with no one, no one can compete with her. --}}
 </div>
