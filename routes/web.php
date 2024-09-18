dd340f2659df105838935dd2ca2ba5c0cb0687f8
Igiz <igizstarkle@gmail.com>
2024-09-18 13:07:50 +0300

    updated post route

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Livewire\Posts;
+
 Route::get('/', function () {
     return view('welcome');
 });
@@ -15,3 +17,5 @@ Route::middleware([
         return view('dashboard');
     })->name('dashboard');
 });
+
+Route::get('posts', Posts::class)->middleware('auth');
